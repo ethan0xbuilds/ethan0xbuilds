f9c5154ecf25c620bb8b019a78b5e2b129a39f4c
Ethan <ethan0xbuilds@proton.me>
2026-04-04 21:31:26 +0800

    fix: remove sitemap integration (causes build crash)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
   output: 'static',
   site: 'https://me.oasaka.xyz',
-  integrations: [sitemap()],
 });
